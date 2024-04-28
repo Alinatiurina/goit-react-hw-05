095d2402802ef62be0889885c4d63c4bbfc8118d
Alinatiurina <144820507+Alinatiurina@users.noreply.github.com>
2024-04-28 16:54:29 +0300

    add notify

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -3,12 +3,16 @@ import css from './MoviesPage.module.css';
 import MovieList from "../../components/MovieList/MovieList";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function MoviesPage() {
     const [query, setQuery] = useState(""); 
     const [searchParams, setSearchParams] = useSearchParams();
+    const notify = () => toast.error('Please, enter your request!');
+  
 
     const handleSearch = (query) => {
+        if (query === "") { notify() }
         setQuery(query);
         setSearchParams({ query: query });
     };
@@ -18,7 +22,7 @@ export default function MoviesPage() {
             <Formik
                 initialValues={{ query: "" }}
                 onSubmit={(values, actions) => {
-                    handleSearch(values.query);
+                    handleSearch(values.query.trim().toLocaleLowerCase());
                     actions.resetForm();
                 }}
             >
@@ -29,6 +33,7 @@ export default function MoviesPage() {
                         placeholder="Search movies"
                     />
                     <button type="submit" className={css.button}>Search</button>
+                <Toaster position="top-right" reverseOrder={false} />
                 </Form>
             </Formik>
             {query && <MovieList moviesQuery={query} />}
